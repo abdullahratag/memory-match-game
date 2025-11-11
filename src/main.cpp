1442e114ee6b2d57f0fa00ef49e07449d90e8ce8
Abdullah Ratag <co240280@adzu.edu.ph>
2025-11-11 22:07:09 +0800

    core gameplay loop with card flipping

--- src/main.cpp
+++ src/main.cpp
@@ -123,7 +123,13 @@ int main(void)
     SetTargetFPS(60);
     GameScreen currentScreen = MENU;
     GameBoard* board = nullptr;
+    Stack flippedCardsStack;
     int gameDimension = 0;
+    int turns = 0;
+    int matchesFound = 0;
+    float flipTimer = 0.0f;
+    const float FLIP_DELAY = 1.0f;
+    
 
     while (!WindowShouldClose()) 
     {
@@ -140,7 +146,52 @@ int main(void)
                         currentScreen = GAMEPLAY;
                     }
                 } break;
-                case GAMEPLAY: {} break; 
+                case GAMEPLAY: {
+                    if(flipTimer > 0 ){
+                        flipTimer -= GetFrameTime();
+                        if(flipTimer <= 0) {
+                            CardPosition pos1 = flippedCardsStack.pop(); 
+                            CardPosition pos2 = flippedCardsStack.pop();
+                            board->cards[pos1.row][pos1.col].isFlipped = false;
+                            board->cards[pos2.row][pos2.col].isFlipped = false;
+                        }
+                    } else{
+                        if(flippedCardsStack.size() < 2  && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                            int boardSizePixels = 480;
+                            int cardSize = boardSizePixels / gameDimension;
+                            int offsetX = ( screenWidth - boardSizePixels) / 2;
+                            int offsetY = (screenHeight - boardSizePixels) / 2;
+                            
+                            if(CheckCollisionPointRec(mousePoint, {(float)offsetX, (float)offsetY, (float)boardSizePixels,(float)boardSizePixels})) {
+                                int col = (mousePoint.x - offsetX) / cardSize;
+                                int row = (mousePoint.y - offsetY) / cardSize;
+                                if(!board->cards[row][col].isMatched && !board->cards[row][col].isFlipped) {
+                                    board->cards[row][col].isFlipped = true;
+                                    flippedCardsStack.push(row, col);
+                                    if(flippedCardsStack.size() == 2){
+                                        turns++;
+                                        CardPosition pos1 = flippedCardsStack.peek(); 
+                                        CardPosition pos2 = flippedCardsStack.peekSecond();
+
+                                        if(board->cards[pos1.row][pos1.col].value == board->cards[pos2.row][pos2.col].value){
+                                            board->cards[pos1.row][pos1.col].isMatched = true; 
+                                            board->cards[pos2.row][pos2.col].isMatched = true;
+
+                                            matchesFound++;
+                                            flippedCardsStack.pop();
+                                            flippedCardsStack.pop();
+                                            if(board->isGameOver()) {
+                                                currentScreen = GAME_OVER; 
+                                            }
+                                        } else {
+                                            flipTimer = FLIP_DELAY;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } break; 
                 case GAME_OVER: {} break;
             }
         }
@@ -154,7 +205,33 @@ int main(void)
                 DrawRectangle(250, 340, 300, 50, LIGHTGRAY); DrawText("Hard", 335, 355, 20, BLACK);
                 DrawRectangle(250, 410, 300, 50, MAROON); DrawText("Quit", 370, 425, 20, WHITE);
             } break;
-            case GAMEPLAY: {} break;
+            case GAMEPLAY: {
+                int boardSizePixels = 480;
+                int cardSize = boardSizePixels / gameDimension;
+                int padding = 5;
+                int offsetX = (screenWidth - boardSizePixels) / 2;
+                int offsetY = (screenHeight - boardSizePixels) / 2;
+
+                for (int r = 0; r < gameDimension; ++r){
+                    for (int c = 0; c < gameDimension; ++c){
+                        Rectangle cardRect = {(float)(offsetX + c * cardSize + padding), 
+                        (float)(offsetY + r * cardSize + padding), (float)(cardSize - 2 * padding), (float)(cardSize - 2 * padding)};
+
+                        if(board->cards[r][c].isFlipped || board->cards[r][c].isMatched) {
+                            DrawRectangleRec(cardRect, WHITE);
+                            DrawRectangleLinesEx(cardRect, 2, board->cards[r][c].isMatched ? GREEN : BLACK);
+                            std::string val(1, board->cards[r][c].value);
+                            int fontSize = cardSize / 2;
+                            DrawText(val.c_str(), cardRect.x + cardRect.width / 4,  cardRect.y + cardRect.height / 2 - fontSize / 2, fontSize, BLACK);
+                        } else {
+                            DrawRectangleRec(cardRect, BLUE);
+                        }
+                    }
+                }
+                DrawText(TextFormat("Turns: %d", turns), 20, 20, 30, DARKGRAY);
+                int totalPairs = (gameDimension * gameDimension) / 2;
+                DrawText(TextFormat("Matches: %d / %d", matchesFound, totalPairs), screenWidth - 250, 20, 30, DARKGRAY);
+            } break;
             case GAME_OVER: {} break;
         }
         EndDrawing();
